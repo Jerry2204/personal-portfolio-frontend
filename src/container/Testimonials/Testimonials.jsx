e14e393b430495e66c17b8750219a7edd834bb07
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-03-04 23:38:53 +0700

    progress testimonials

--- src/container/Testimonials/Testimonials.jsx
+++ src/container/Testimonials/Testimonials.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client, urlFor } from '../../client';
+
 import './Testimonial.scss';
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = `*[_type == "brands"]`;
+    const testimonialsQuery = `*[_type == "testimonials"]`;
+
+    client.fetch(testimonialsQuery).then((data) => {
+      setTestimonials(data);
+    });
+
+    client.fetch(query).then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img
+              src={urlFor(testimonials[currentIndex].imageurl)}
+              alt="testimonials"
+            />
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
-export default Testimonials;
+export default AppWrap(
+  MotionWrap(Testimonials, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg'
+);
